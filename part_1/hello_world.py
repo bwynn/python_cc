5e0e7bd7d9a02b513e3ed9c4978b26a1828f232e
Brian Wynn <b_wynn@apple.com>
2016-02-10 16:52:57 -0800

    Initial commit. Teaching myself from the very basics all things python.

---
+++ part_1/hello_world.py
@@ -0,0 +1,46 @@
+# print a string
+# print("Hello Python World!");
+
+# define a variable
+# message = "Hello Python World!"
+# print(message)
+
+# serve string as a title
+# name = "han solo"
+# print(name.title())
+
+# subsequently, upper and lowercase
+# name = "Han Solo"
+# print(name.upper())
+# print(name.lower())
+
+# concatenation
+# first_name = "han"
+# last_name = "solo"
+# full_name = first_name + " " + last_name
+# message = "Hello, " + full_name.title() + "!"
+# print(message)
+
+################################################################################
+
+# adding tabs and whitespaces using \t
+# print("Python")
+# print("\tPython")
+
+# newlines using \n
+# print("Languages:\nPython\nC\nJavaScript")
+
+# adding them together
+# print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+################################################################################
+
+# stripping whitespace from right hand side of string
+# favorite_language = "python "
+# print(favorite_language.rstrip())
+
+# stripping whitespace from left hand side of string
+# favorite_language = " python"
+# print(favorite_language.lstrip())
+
+# favorite_language.strip() will remove any additional whitespaces 
