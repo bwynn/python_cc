2f1250d49489d070782dfc7a7c00d6a580f56444
Brian Wynn <b_wynn@apple.com>
2016-02-12 15:10:47 -0800

    working through dictionaries, which I know as objects, while waiting for work.

---
+++ part_1/dictionaries/alien.py
@@ -0,0 +1,34 @@
+# alien_0 = {'color': 'green', 'points': 5}
+
+# print(alien_0['color'])
+# print(alien_0['points'])
+
+# new_points = alien_0['points']
+# print("You just earned " + str(new_points) + " points!")
+
+# alien_0['x-position'] = 0
+# alien_0['y-position'] = 25
+
+# print(alien_0)
+
+################################################################################
+# starting from scratch
+# alien_0 = {}
+
+# alien_0['color'] = 'green'
+# alien_0['points'] = 5
+
+# print(alien_0)
+
+################################################################################
+# modifying values
+# alien_0 = {'color': 'green'}
+# print("The alien is " + alien_0['color'] + ".")
+
+# alien_0['color'] = 'yellow'
+# print("The alien is " + alien_0['color'] + ".")
+
+################################################################################
+# a more complex implementation
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print("Original x-position: " + str(alien_0['x_position']))
