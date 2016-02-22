73b5952c341d31284e3175518640bf6fd709906c
Brian Wynn <b_wynn@apple.com>
2016-02-22 15:19:46 -0800

    Added a few additional examples, including importing modules and module functions.

---
+++ part_1/functions/making_pizzas.py
@@ -0,0 +1,11 @@
+#imports an entire module
+# import pizza
+
+# pizza.make_pizza(16, 'pepperoni')
+# pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+#importing functions from a module
+from pizza import make_pizza
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
