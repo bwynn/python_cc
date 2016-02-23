15b890b901b20501a329ce1ef954ef4888818f93
Brian Wynn <b_wynn@apple.com>
2016-02-22 16:47:01 -0800

    Moved onto classes chapter. Going over some of the specifics for creating child classes.

--- part_1/functions/making_pizzas.py
+++ part_1/functions/making_pizzas.py
@@ -5,7 +5,13 @@
 # pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
 
 #importing functions from a module
-from pizza import make_pizza
+# from pizza import make_pizza
 
-make_pizza(16, 'pepperoni')
-make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+# make_pizza(16, 'pepperoni')
+# make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+# giving an imported function an alias
+from pizza import make_pizza as mp
+
+mp(16, 'pepperoni')
+mp(12, 'mushrooms', 'green peppers', 'extra cheese')
