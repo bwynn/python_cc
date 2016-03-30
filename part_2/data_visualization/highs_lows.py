49bdb1b5e11d827b794a9eaf391606f99a01e594
Brian Wynn <b_wynn@apple.com>
2016-03-30 16:51:54 -0700

    Began chapter covering data visualizations using csv input files.

---
+++ part_2/data_visualization/highs_lows.py
@@ -0,0 +1,26 @@
+import csv
+
+from matplotlib import pyplot as plt
+
+# get high temperatures from file
+filename = 'sitka_weather_2014.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    highs = []
+    for row in reader:
+        high = int(row[1])
+        highs.append(high)
+
+# plot data
+fig = plt.figure(figsize=(10, 6))
+plt.plot(highs, c='red')
+
+# format plot
+plt.title("Daily high temperatures, 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
